4f57d978f7a47247cbf75088a81d7619137fd69d
Anna Perevedentseva <anna.pereved@gmail.com>
2024-03-08 16:40:44 +0100

    feat: read the value of port from .env file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { resolve } from 'path';
 import { readFile } from 'fs/promises';
@@ -8,10 +9,13 @@ import { load } from 'js-yaml';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  config();
+  const PORT = process.env.PORT || 4000;
+
   const doc = await readFile(resolve(__dirname, '../doc/api.yaml'), 'utf8');
   const docObject = load(doc);
   SwaggerModule.setup('doc', app, docObject as OpenAPIObject);
 
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
