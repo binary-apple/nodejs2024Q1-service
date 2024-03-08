08bbedba0deeab6e399da1c6905678f9e4bca683
Anna Perevedentseva <anna.pereved@gmail.com>
2024-03-08 14:14:18 +0100

    feat: configure swagger with openAPI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
+import { load } from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const doc = await readFile(resolve(__dirname, '../doc/api.yaml'), 'utf8');
+  const docObject = load(doc);
+  SwaggerModule.setup('doc', app, docObject as OpenAPIObject);
+
   await app.listen(4000);
 }
 bootstrap();
