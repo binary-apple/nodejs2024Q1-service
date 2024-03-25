1d7290c630f3ba9811e823b88074cdd569d560de
Anna Perevedentseva <anna.pereved@gmail.com>
2024-03-25 19:01:07 +0100

    fix: remove album store

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
-import { AlbumStore } from './album_store/album_store';
 import { TrackModule } from 'src/track/track.module';
 import { Album } from './entities/album.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [AlbumController],
-  providers: [AlbumService, AlbumStore],
+  providers: [AlbumService],
   exports: [AlbumService],
   imports: [TypeOrmModule.forFeature([Album]), TrackModule],
 })
