473d7b889adafb682eb013537916174de8164929
Anna Perevedentseva <anna.pereved@gmail.com>
2024-03-25 23:09:02 +0100

    fix: doc

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-// import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
-// import { resolve } from 'path';
-// import { readFile } from 'fs/promises';
-// import { load } from 'js-yaml';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
+import { load } from 'js-yaml';
 import 'reflect-metadata';
 
 async function bootstrap() {
@@ -15,9 +15,9 @@ async function bootstrap() {
   config();
   const PORT = process.env.PORT || 4000;
 
-  // const doc = await readFile(resolve(__dirname, '../doc/api.yaml'), 'utf8');
-  // const docObject = load(doc);
-  // SwaggerModule.setup('doc', app, docObject as OpenAPIObject);
+  const doc = await readFile(resolve(__dirname, '../doc/api.yaml'), 'utf8');
+  const docObject = load(doc);
+  SwaggerModule.setup('doc', app, docObject as OpenAPIObject);
 
   await app.listen(PORT);
 }
